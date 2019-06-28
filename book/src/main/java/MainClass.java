814313d0da85c91dd300f571422103ddc9c59dc0
blindt <blindt@o2.pl>
2019-06-29 01:45:22 +0200

    save an old structure of mainclass

--- book/src/main/java/MainClass.java
+++ book/src/main/java/MainClass.java
@@ -1,38 +1,49 @@
 import java.awt.Rectangle;
-import gui.GuestMenu;
-import gui.LoggedMenu;
-import gui.BaseMenu;
+import java.util.List;
+
+import gui.*;
+import database.*;
+import mechanics.*;
 
 public class MainClass {
+	public static void main(String[] args){
+		
+		// Przykładowe użycie ResourceManagera, żeby otrzymać przepisy z pliku .xml
+		// Plik w którym przechowujemy przepisy: book/src/main/resources/recipes.xml
+		ResourceManager resource_manager = new ResourceManager();
+		List<Recipe> recipes = resource_manager.fetchRecipes();
 	
-	public static void main(String[] args)
-	{
-		Rectangle dimensions = new Rectangle(300, 100, 1100, 900);;
+		Rectangle dimensions = new Rectangle(300, 100, 1100, 900);
+
 		while(true) 
 		{
 			BaseMenu object = new GuestMenu(dimensions);
+			System.out.println(object);
+			BaseMenu.main_frame.getContentPane().repaint();
 			while(object.get_logged() == false)
 			{
 				if(object.get_logged() == true)
 				{
 					break;
 				}
-				object.extra_things();
+				object.extraThings();
 			}
 			dimensions = BaseMenu.main_frame.getBounds();
 			object = (BaseMenu)new LoggedMenu(dimensions);
+			BaseMenu.main_frame.getContentPane().repaint();
+			System.out.println(object);
 			while(object.get_logged() == true)
 			{
 				if(object.get_logged() == false)
 				{
 					break;
 				}
-				object.extra_things();
+				object.extraThings();
 			}
 			dimensions = BaseMenu.main_frame.getBounds();
 		}
-		
 	}
 }
+
 
 
